9f894d29a59ef5cfd8b302151b6b4925d87d17be
Swetanshu Pandey <swetanshupandey1722@gmail.com>
2023-10-10 20:05:03 +0530

    data ingestion

---
+++ src/cnnClassifier/components/data_ingestion.py
@@ -0,0 +1,23 @@
+import os
+import zipfile
+import gdown
+from cnnClassifier import logger
+from cnnClassifier.utils.common import get_size
+
+class DataIngestion:
+    def __init__(self,config:DataIngestionConfig):
+        self.config = config
+    
+    def download_file(self) -> str:
+        try:
+            dataset_url = self.config.source_URL
+            zip_download_dir =  self.config.local_data_file
+            os.makedirs("artifacts/data_ingestion",exist_ok=True)
+            logger.info(f"Downloading data from {dataset_url} into file {zip_download_dir}")
+            file_id = dataset_url.split("/")[-2]
+            prefix = 'https://drive.google.com/uc?/export=download&id='
+            gdown.download(prefix+file_id,zip_download_dir)
+            logger.info(f"Downloaded data from {dataset_url} into file {zip_download_dir}")
+            
+            
+                        
