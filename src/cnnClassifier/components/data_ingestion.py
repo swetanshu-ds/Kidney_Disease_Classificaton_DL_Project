a50fed43a129df3569aa69d629340f15a4f31ef8
Swetanshu Pandey <swetanshupandey1722@gmail.com>
2023-10-10 20:08:59 +0530

    DI

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -3,6 +3,7 @@ import zipfile
 import gdown
 from cnnClassifier import logger
 from cnnClassifier.utils.common import get_size
+from cnnClassifier.entity.config_entity import (DataIngestionConfig)
 
 class DataIngestion:
     def __init__(self,config:DataIngestionConfig):
